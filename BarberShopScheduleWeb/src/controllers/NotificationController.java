6539d279563dd4c2f708e052894c76cec5636c31
Alex Ghenghiu <alexgg94@hotmail.com>
2018-01-12 17:54:02 +0100

    Added accepted appointment push notification

--- BarberShopScheduleWeb/src/controllers/NotificationController.java
+++ BarberShopScheduleWeb/src/controllers/NotificationController.java
@@ -38,6 +38,7 @@ public class NotificationController {
 	@Consumes("application/json")
 	public boolean cancelAppointment(@PathParam("id") int id, @PathParam("token") String token) {
 
+		HttpURLConnection conn = null;
 		Connection connection = null;
 		Statement stm = null;
 		String strEstat = new String("ok");
@@ -69,36 +70,46 @@ public class NotificationController {
 
 								try {
 
-									URL url = new URL("https://fcm.googleapis.com/fcm/send");
+									ResultSet service_rs = stm.executeQuery(
+											"SELECT * FROM service WHERE id = " + appointment_rs.getInt(4));
 
-									HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-									conn.setDoOutput(true);
-									conn.setRequestMethod("POST");
-									conn.setRequestProperty("Content-Type", "application/json");
-									conn.setRequestProperty("Authorization",
-											"server_token");
+									if (service_rs.next()) {
+										URL url = new URL("https://fcm.googleapis.com/fcm/send");
 
-									String input = "{\"to\":\"" + deviceToken
-											+ "\", \"data\":{\"type\" : \"cancel\", \"service\" : \"Kid haircut\", \"time\" : \"2018-01-11 15:20\" }}";
+										conn = (HttpURLConnection) url.openConnection();
+										conn.setDoOutput(true);
+										conn.setRequestMethod("POST");
+										conn.setRequestProperty("Content-Type", "application/json");
+										conn.setRequestProperty("Authorization",
+												"server_token");
 
-									OutputStream os = conn.getOutputStream();
-									os.write(input.getBytes());
-									os.flush();
+										String input = "{\"to\":\"" + deviceToken
+												+ "\", \"data\":{\"type\" : \"cancel\", \"service\" : \""
+												+ service_rs.getString(3) + "\", \"time\" : \""
+												+ appointment_rs.getString(6) + "\" }}";
 
-									BufferedReader br = new BufferedReader(
-											new InputStreamReader((conn.getInputStream())));
+										OutputStream os = conn.getOutputStream();
+										os.write(input.getBytes());
+										os.flush();
 
-									String output;
-									while ((output = br.readLine()) != null) {
-										System.out.println("\nClient POST. Answer: " + output);
+										BufferedReader br = new BufferedReader(
+												new InputStreamReader((conn.getInputStream())));
+
+										String output;
+										while ((output = br.readLine()) != null) {
+											System.out.println("\nClient POST. Answer: " + output);
+										}
+
+										conn.disconnect();
 									}
-
-									conn.disconnect();
 
 								} catch (MalformedURLException e) {
 									e.printStackTrace();
 								} catch (IOException e) {
 									e.printStackTrace();
+								} finally {
+									if (conn != null)
+										conn.disconnect();
 								}
 							}
 
@@ -135,10 +146,11 @@ public class NotificationController {
 
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/requestAppointment/{id}/{token}")
+	@Path("/acceptAppointment/{id}/{token}")
 	@Consumes("application/json")
-	public boolean requestAppointment(@PathParam("id") int id, @PathParam("token") String token) {
+	public boolean acceptAppointment(@PathParam("id") int id, @PathParam("token") String token) {
 
+		HttpURLConnection conn = null;
 		Connection connection = null;
 		Statement stm = null;
 		String strEstat = new String("ok");
@@ -163,46 +175,167 @@ public class NotificationController {
 
 						if (appointment_rs.next()) {
 							ResultSet client_rs = stm
-									.executeQuery("SELECT * FROM barbershop WHERE id = " + appointment_rs.getInt(3));
+									.executeQuery("SELECT * FROM client WHERE id = " + appointment_rs.getInt(2));
 
 							if (client_rs.next()) {
-								String deviceToken = client_rs.getString(11);
+								String deviceToken = client_rs.getString(8);
 
 								try {
 
-									URL url = new URL("https://fcm.googleapis.com/fcm/send");
+									ResultSet service_rs = stm.executeQuery(
+											"SELECT * FROM service WHERE id = " + appointment_rs.getInt(4));
 
-									HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-									conn.setDoOutput(true);
-									conn.setRequestMethod("POST");
-									conn.setRequestProperty("Content-Type", "application/json");
-									conn.setRequestProperty("Authorization",
-											"server_token");
+									if (service_rs.next()) {
+										URL url = new URL("https://fcm.googleapis.com/fcm/send");
 
-									String input = "{\"to\":\"" + deviceToken
-											+ "\", \"data\":{\"type\" : \"request\", \"service\" : \"Kid haircut\", \"time\" : \"2018-01-11 15:20\" }}";
+										conn = (HttpURLConnection) url.openConnection();
+										conn.setDoOutput(true);
+										conn.setRequestMethod("POST");
+										conn.setRequestProperty("Content-Type", "application/json");
+										conn.setRequestProperty("Authorization",
+												"server_token");
 
-									OutputStream os = conn.getOutputStream();
-									os.write(input.getBytes());
-									os.flush();
+										String input = "{\"to\":\"" + deviceToken
+												+ "\", \"data\":{\"type\" : \"accept\", \"service\" : \""
+												+ service_rs.getString(3) + "\", \"time\" : \""
+												+ appointment_rs.getString(6) + "\" }}";
 
-									BufferedReader br = new BufferedReader(
-											new InputStreamReader((conn.getInputStream())));
+										OutputStream os = conn.getOutputStream();
+										os.write(input.getBytes());
+										os.flush();
 
-									String output;
-									while ((output = br.readLine()) != null) {
-										System.out.println("\nClient POST. Answer: " + output);
+										BufferedReader br = new BufferedReader(
+												new InputStreamReader((conn.getInputStream())));
+
+										String output;
+										while ((output = br.readLine()) != null) {
+											System.out.println("\nClient POST. Answer: " + output);
+										}
+
+										conn.disconnect();
 									}
-
-									conn.disconnect();
 
 								} catch (MalformedURLException e) {
 									e.printStackTrace();
 								} catch (IOException e) {
 									e.printStackTrace();
+								} finally {
+									if (conn != null)
+										conn.disconnect();
 								}
 							}
 
+						}
+
+						stm.executeUpdate("UPDATE appointment SET pendingconfirmation = 0 WHERE id = " + id);
+
+						result = true;
+					}
+				}
+			}
+		}
+
+		catch (Exception e) {
+			e.printStackTrace();
+			strEstat = "status ko due to -> " + e.getMessage();
+		} finally {
+			try {
+				if (connection != null) {
+					connection.close();
+				}
+
+				if (stm != null) {
+					stm.close();
+				}
+			}
+
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/requestAppointment/{id}/{token}")
+	@Consumes("application/json")
+	public boolean requestAppointment(@PathParam("id") int id, @PathParam("token") String token) {
+
+		HttpURLConnection conn = null;
+		Connection connection = null;
+		Statement stm = null;
+		String strEstat = new String("ok");
+		boolean result = false;
+
+		try {
+			InitialContext cxt = new InitialContext();
+			if (cxt != null) {
+				DataSource ds = (DataSource) cxt.lookup("java:jboss/PostgresXA");
+
+				if (ds == null)
+					strEstat = "Error al crear el datasource";
+				else {
+					connection = ds.getConnection();
+					stm = connection.createStatement();
+
+					ResultSet session = stm.executeQuery("SELECT * FROM session WHERE session_token = '" + token + "'");
+
+					if (session.next()) {
+
+						ResultSet appointment_rs = stm.executeQuery("SELECT * appointment WHERE id = " + id);
+
+						if (appointment_rs.next()) {
+							ResultSet client_rs = stm
+									.executeQuery("SELECT * FROM client WHERE id = " + appointment_rs.getInt(2));
+
+							if (client_rs.next()) {
+								String deviceToken = client_rs.getString(8);
+
+								try {
+
+									ResultSet service_rs = stm.executeQuery(
+											"SELECT * FROM service WHERE id = " + appointment_rs.getInt(4));
+
+									if (service_rs.next()) {
+										URL url = new URL("https://fcm.googleapis.com/fcm/send");
+
+										conn = (HttpURLConnection) url.openConnection();
+										conn.setDoOutput(true);
+										conn.setRequestMethod("POST");
+										conn.setRequestProperty("Content-Type", "application/json");
+										conn.setRequestProperty("Authorization",
+												"server_token");
+
+										String input = "{\"to\":\"" + deviceToken
+												+ "\", \"data\":{\"type\" : \"request\", \"service\" : \""
+												+ service_rs.getString(3) + "\", \"time\" : \""
+												+ appointment_rs.getString(6) + "\" }}";
+
+										OutputStream os = conn.getOutputStream();
+										os.write(input.getBytes());
+										os.flush();
+
+										BufferedReader br = new BufferedReader(
+												new InputStreamReader((conn.getInputStream())));
+
+										String output;
+										while ((output = br.readLine()) != null) {
+											System.out.println("\nClient POST. Answer: " + output);
+										}
+
+										conn.disconnect();
+									}
+
+								} catch (MalformedURLException e) {
+									e.printStackTrace();
+								} catch (IOException e) {
+									e.printStackTrace();
+								} finally {
+									if (conn != null)
+										conn.disconnect();
+								}
+							}
 						}
 
 						result = true;
